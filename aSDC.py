ad7cc71174b268268c663b353e5faf567da53ac4
Матвей <matveykormich03@gmail.com>
2023-03-26 09:13:45 +0300

    update

--- aSDC.py
+++ aSDC.py
@@ -1,21 +1,21 @@
 class Student:
-    def __init__(self, name: str, age: int = 0, country: str = 'Bulgaria',) -> None:
+    def __init__(self, name: str,name2: str, age: int = 0, country: str = 'Bulgaria') -> None:
         self.name = name
         self.age = age
         self.country = country
-        self.neme = name
+        self.name2 = name2
 
 
 
     def __str__(self):
-        return f'Hello {self.neme}! My name is {self.name}. I am {self.age} years old. I am from {self.country}.'
+        return f'Hello {self.name2}! My name is {self.name}. I am {self.age} years old. I am from {self.country}.'
 
     def print_data(self) -> None:
-        print(f'Hello {self.neme}! My name is {self.name}. I am {self.age} years old. I am from {self.country}.')
+        print(f'Hello {self.name2}! My name is {self.name}. I am {self.age} years old. I am from {self.country}.')
 
-first_student = Student(name ='Susan', age=31, country= 'Italy')
-second_student = Student('Bob', 64, 'France')
-third_student = Student('Tom', age=26)
+first_student = Student(name ='Susan',name2 = 'Bob', age=31, country= 'Italy')
+second_student = Student('Bob','Susan', 64, 'France')
+third_student = Student('Tom','Bob' ,age=26)
 
 print(first_student)
 print(second_student)
