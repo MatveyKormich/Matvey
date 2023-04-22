eb0219f0dbe26c0f88fb27fcd84038e4ac6a836f
Матвей <matveykormich03@gmail.com>
2023-04-22 18:20:59 +0300

    update

--- ertyubn.py
+++ ertyubn.py
@@ -6,27 +6,19 @@ from bs4 import BeautifulSoup
 
 from datetime import datetime
 
-conn = sqlite3.connect('weather.db')
+conn = sqlite3.connect('weather')
 
 c = conn.cursor()
 
-c.execute('''CREATE TABLE IF NOT EXISTS weather
+c.execute('''CREATE TABLE IF NOT EXISTS weather(date DATE,time TIME,temperature REAL)''')
 
-            (id INTEGER PRIMARY KEY AUTOINCREMENT,
-
-            date DATE,
-
-            time TIME,
-
-            temperature REAL)''')
-
-url = 'https://www.gismeteo.ua/ua/weather-kamyanske-11850/'
+url = 'https://meteopost.com/city/28368/'
 
 response = requests.get(url)
 
 soup = BeautifulSoup(response.text, 'html.parser')
 
-temperature = float(soup.find('span', class_='js_value tab-weather__value_l').text)
+temperature = float(soup.find('span', class_='t').text)
 
 now = datetime.now()
 
@@ -39,4 +31,25 @@ c.execute('INSERT INTO weather (date, time, temperature) VALUES (?, ?, ?)', (dat
 conn.commit()
 
 conn.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#id INTEGER PRIMARY KEY AUTOINCREMENT,
 
