3f201ecee78d3c46ebcc971c572ba0641a75647a
Матвей <matveykormich03@gmail.com>
2023-04-29 18:28:05 +0300

    update

--- TP.py
+++ TP.py
@@ -1,6 +1,20 @@
 import random
 
+print('')
 
+fog = input('Что даш коту: ')
+def eat(function):
+    def wrapped(*args):
+        for arg in args:
+            if not isinstance(arg, fog):
+                raise ValueError('Кот останется без еды ')
+        return function(*args)
+
+    return wrapped
+
+
+
+@eat
 
 
 class Dog:
--- lesson 11/sertyj.py
+++ lesson 11/sertyj.py
@@ -10,19 +10,39 @@ image = cv2.imread(image_path)
 
 cat_face = cat_face_cascade.detectMultiScale(image)
 
+cat_mouth = cat_face_cascade.detectMultiScale(image)
+
 cat = Image.open(image_path)
 glasses = Image.open("glasses.png")
+maska = Image.open("maska1.png")
 
 cat = cat.convert("RGBA")
 glasses = glasses.convert("RGBA")
+maska = maska.convert("RGBA")
 
 for (x, y, w, h) in cat_face:
     glasses = glasses.resize((w, int(h/3)))
+
+    maska = maska.resize((w, int(h/3)))
+
     cat.paste(glasses, (x, int(y+h/4)), glasses)
+
+    cat.paste(maska, (x, int(y+h/4)), maska)
+
+    cat.save("cat_with_maska1")
+
     cat.save("cat_with_glasses.png")
+
     cat_with_glasses = cv2.imread("cat_with_glasses.png")
+
+    cat_with_maska = cv2.imread("cat_with_maska.png")
+
     cv2.imshow("Cat_with_glasses", cat_with_glasses)
+
+    cv2.imshow("Cat_with_maska", cat_with_maska)
+
     cv2.waitKey()
+
 
 
 
